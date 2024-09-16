80b034cf8edb6013fd9390bc183f74c49bad0b60
Одногулов Руслан Хосянович <orh@cds.guru>
2024-09-16 12:01:46 +0300

    first commit

---
+++ home/bitrix/www/index.php
@@ -0,0 +1,32 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("title", "Строительные смеси в #REGION_NAME_DECLINE_PP# - официальный сайт GLIMS (ГЛИМС)");
+$APPLICATION->SetPageProperty("keywords", "сухие строительные смеси от производителя завод производство");
+$APPLICATION->SetPageProperty("description", "Официальный сайт производителя строительных смесей премиум-класса GLIMS. Тел. 8 (495) 510-4355");
+$APPLICATION->SetPageProperty("viewed_show", "Y");
+$APPLICATION->SetTitle("Производитель строительных смесей GLIMS ");
+?>
+<script type="application/ld+json">
+    {
+        "@context": "http://schema.org/",
+        "@type": "BreadcrumbList",
+        "itemListElement": [{
+        "@type": "ListItem",
+        "position": 1,
+        "item": {
+        "@id": "https://glims.ru/",
+        "name": "РњР°С‚РµСЂРёР°Р»С‹ РґР»СЏ СЃС‚СЂРѕРёС‚РµР»СЊСЃС‚РІР°"
+    }
+},{
+"@type": "ListItem",
+"position": 2,
+"item": {
+"@id": "https://glims.ru/#",
+"name": "&#128736;&#65039; &#128736;&#65039; &#128736;&#65039;"
+}
+}]
+}
+</script>
+<div class="AFTER_SLIDER maxwidth-theme">
+	<h1 style="text-align: center;">Производитель строительных смесей GLIMS</h1>
+</div>
+ <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
